6dc309e5c29d94c568ff9be002c26b501c7b8743
sanaa semlali <sanaa.semlali@gmail.com>
2023-03-30 10:41:16 +0000

    implemented pundit
    Signed-off-by: sanaa semlali <sanaa.semlali@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
 
   # Pundit: allow-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  #after_action : avant de rendre la view
-  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -19,14 +18,4 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-  #this method checks if it can skip its authorization checks
-    #if
-    #the current controller inherit from devise::sessionsController => true
-    # if
-    # the current controller's name matches one of the following regular expressions:
-        #^(rails_)?admin == names start with admin or rails_admin
-        # (^pages$) == matches controller with the name of pages
-    ## return true
-  # if neither of this conditions are met (returns false)
-  # Pundit should perform its authorization checks
 end
