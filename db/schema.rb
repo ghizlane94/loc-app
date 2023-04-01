245c6805a39918944e6463f896e1adeef00dd11f
Afafe Slimani <slimani.afaf.iaa@gmail.com>
2023-04-01 11:43:55 +0100

    deleted photo from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_103931) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_01_104159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_103931) do
     t.integer "year"
     t.integer "mileage"
     t.datetime "status"
-    t.string "photo"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "destroying motos"
+puts "destroying motos "
 Moto.destroy_all
 puts "destroting users"
 User.destroy_all
