5cd64ecfb9afea31ea1053680b6593ef7533ff14
Afafe Slimani <slimani.afaf.iaa@gmail.com>
2023-04-01 12:39:20 +0100

    better seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_01_104159) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_01_113434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_01_104159) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "city"
+    t.string "image_url"
     t.index ["user_id"], name: "index_motos_on_user_id"
   end
 
