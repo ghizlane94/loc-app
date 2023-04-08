2d32241dc611ae2fcf9e08c2513d6d0c8d07d8de
sanaa semlali <sanaa.semlali@gmail.com>
2023-04-08 09:17:25 +0000

    @reser instead of resa ?

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,7 +4,9 @@ class ReservationsController < ApplicationController
   end
 
   def show
-    @reservation= Reservation.find(params[:id])
+    @reservation = Reservation.find(params[:id])
+    @start_time = @reservation.start_time
+    @end_time = @reservation.end_time
   end
 
   def new
@@ -12,13 +14,13 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    resa = Reservation.new(
-      start: params[:reservation][:start],
-      end: params[:reservation][:end],
+    @reservation = Reservation.new(
+      start_time: params[:reservation][:start_time],
+      end_time: params[:reservation][:end_time],
       moto_id: params[:moto_id]
     )
     # resa.save()
-    resa.user = current_user
-    redirect_to reservation_path(resa) if resa.save!
+    @reservation.user = current_user
+    redirect_to reservation_path(resa) if @reservation.save!
   end
 end
