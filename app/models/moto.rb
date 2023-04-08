5cc50336e6a20930b3bb11446b10700ee6521d0e
sanaa semlali <sanaa.semlali@gmail.com>
2023-04-08 09:16:57 +0000

    implemented available_dates meth

--- app/models/moto.rb
+++ app/models/moto.rb
@@ -2,4 +2,13 @@ class Moto < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
   has_many :reservations, dependent: :destroy
+
+  def available_dates
+    next_reservation = reservations.where("start_time > ? AND end_time > ? ", Date.today, Date.today + 30).order(start_time: :asc).first
+    #return a list of reservation which starts in the future from today actually and finish 30 days later
+    
+    if next_reservation.nil?
+      return start_time == Date.today && end_time == Date.today + 30
+    end
+  end
 end
