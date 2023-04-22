d3ed20aaf61d58d3a36376a413ede12521d32e8c
ghizlane ait daddaali <ghizlane.aitdaddaali94@gmail.com>
2023-04-22 21:43:18 +0000

    adding cancel to reservation

--- app/javascript/controllers/reservation_controller.js
+++ app/javascript/controllers/reservation_controller.js
@@ -2,18 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reservation"
 export default class extends Controller {
-  static targets = ["firstDiv", "divReservation"]
+  static targets = ["firstDiv", "divReservation", "buttonResa"]
   connect() {
     console.log("iam reservation")
   }
   resa(){
     this.firstDivTarget.style.opacity="0.1";
+    this.buttonResaTarget.style.opacity="0.1";
     this.divReservationTarget.style.display='';
+
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
   }
+  back(){
+    this.firstDivTarget.style.opacity="";
+    this.buttonResaTarget.style.opacity="";
+    this.divReservationTarget.style.display='none';
+
+  }
 }
-
-
